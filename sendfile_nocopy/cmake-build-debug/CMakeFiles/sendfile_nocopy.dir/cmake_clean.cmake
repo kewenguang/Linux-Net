1967060cd1635833923c1366756a16d9d00068dd
kewenguang <2860881669@qq.com>
2018-11-10 22:33:49 +0800

    sendfile_nocopy

---
+++ sendfile_nocopy/cmake-build-debug/CMakeFiles/sendfile_nocopy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sendfile_nocopy.dir/main.cpp.o"
+  "sendfile_nocopy.pdb"
+  "sendfile_nocopy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sendfile_nocopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
