0f4eb7e4972c51e65ddaa5d71f586b5fce238619
kewenguang <2860881669@qq.com>
2018-11-10 21:55:17 +0800

    ss

---
+++ CGI/cmake-build-debug/CMakeFiles/CProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProject.dir/main.cpp.o"
+  "CProject.pdb"
+  "CProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_writev_webhttp/cmake-build-debug/CMakeFiles/test_writev_webhttp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_writev_webhttp.dir/main.cpp.o"
+  "test_writev_webhttp.pdb"
+  "test_writev_webhttp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_writev_webhttp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
