18444ec134309ec5e7c2004f9495667c0d5d08ef
kewenguang <2860881669@qq.com>
2018-11-16 13:13:22 +0800

    et and lt

---
+++ epoll_lt_et/cmake-build-debug/CMakeFiles/epoll_lt_et.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epoll_lt_et.dir/main.cpp.o"
+  "epoll_lt_et.pdb"
+  "epoll_lt_et"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epoll_lt_et.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
