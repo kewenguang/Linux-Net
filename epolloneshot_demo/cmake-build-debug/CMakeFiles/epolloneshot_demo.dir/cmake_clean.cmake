05e884abcc517b3fd5ac5d665d42bdb51dc6a880
kewenguang <2860881669@qq.com>
2018-11-18 18:32:43 +0800

    epolloneshotdemo

---
+++ epolloneshot_demo/cmake-build-debug/CMakeFiles/epolloneshot_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epolloneshot_demo.dir/main.cpp.o"
+  "epolloneshot_demo.pdb"
+  "epolloneshot_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epolloneshot_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
