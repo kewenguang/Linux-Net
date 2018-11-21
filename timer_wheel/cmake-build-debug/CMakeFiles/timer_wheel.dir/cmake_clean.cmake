b95e09b8027fc190998fbeb1f4ffdb19ea0b90e5
kewenguang <2860881669@qq.com>
2018-11-21 20:40:53 +0800

    timer_wheel 可以了

---
+++ timer_wheel/cmake-build-debug/CMakeFiles/timer_wheel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timer_wheel.dir/main.cpp.o"
+  "timer_wheel.pdb"
+  "timer_wheel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timer_wheel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
